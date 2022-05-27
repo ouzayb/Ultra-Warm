b542f03aba4c514d01fd3e55d94cbda31c0eb095
Kaan Karataş <karataskn20@gmail.com>
2022-05-28 02:10:47 +0300

    update

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
     public Transform target; // Target to follow
     public float cameraSpeed; // Speed of Camera/Smoothness
