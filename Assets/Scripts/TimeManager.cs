174178d5f5e2e897f15ba395233dd312253c349d
ouzayb <ouzayb@gmail.com>
2022-05-28 16:05:38 +0300

    cfvbgnsdffg

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    [SerializeField] private float pressedButton;
+    [SerializeField] private float stopTime;
+    [SerializeField] bool stop;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class TimeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (noButtonsPressed())
+        {
+            Time.timeScale = 0;
+            stop = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            stop = false;
+        }
+        //if((stopTime < Time.timeSinceLevelLoad))
+        //Time.timeScale = timeMultiplier;
+    }
+
+    bool noButtonsPressed()
+    {
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical") || Input.GetButton("Fire")) //if any key is pressed(this code is sad bcus I'm baad)
+        {
+            return false;
+        }
+        return true;
     }
 }
