cfb0f839f0a17ab20cb1b0e00c8429b00f1f7c98
ouzayb <ouzayb@gmail.com>
2022-05-29 15:39:36 +0300

    boobiiii

--- Assets/Scripts/HurtTextManager.cs
+++ Assets/Scripts/HurtTextManager.cs
@@ -15,7 +15,7 @@ public class HurtTextManager : MonoBehaviour
 
     public void CreateText(int damage)
     {
-        GameObject DamageText = Instantiate(damageTextPrefab, this.transform.position,new Quaternion(0,0,0,0),parent.transform);
-        DamageText.GetComponent<TextMesh>().text= "-" + damage.ToString() + " seconds lost";
+        GameObject DamageText = Instantiate(damageTextPrefab, new Vector3(transform.position.x - 0.27f, transform.position.y + 0.16f, transform.position.z) ,new Quaternion(0,0,0,0),textParent.transform);
+        DamageText.GetComponent<TextMesh>().text= damage.ToString() + " seconds lost";
     }
 }
